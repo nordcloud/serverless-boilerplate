c2cec7f9fdda2a604fd5a7b49df48115fc594746
Eetu Tuomala <eetu.tuomala@sc5.io>
2016-11-04 23:36:04 +0200

    moves resources to separate file, uses promises in tests

--- fnHello/handler.js
+++ fnHello/handler.js
@@ -1,6 +1,6 @@
 'use strict';
 
 module.exports.hello = (event, context, callback) => {
-  console.log('Got request:', event)
+  console.log('Got request:', event);
   callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
