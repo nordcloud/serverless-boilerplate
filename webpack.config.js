dd930cba81fb7b8de5f872d3e099e6c9a05733e6
Mikael Puittinen <mikael@iki.fi>
2016-11-09 11:31:32 +0200

    Configure webpack automatically based on serverless.yml

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const yaml = require('node-yaml');
+
+function getFunctions() {
+  const serverlessYml = yaml.readSync('serverless.yml');
+  const webPackFunctions = {};
+  const functionNames = Object.keys(serverlessYml.functions || {});
+  functionNames.map((name) => {
+    const handlerFile = serverlessYml.functions[name].handler.replace(/\.[^\.]*$/, '');
+    webPackFunctions[handlerFile] = `./${handlerFile}.js`;
+  });
+  return webPackFunctions;
+}
 
 module.exports = {
-  entry: {
-    'fnHello/handler': './fnHello/handler.js',
-  },
+  entry: getFunctions(),
   target: 'node',
   module: {
     loaders: [
